75991f8cc69dd9542b8a7dd4b16594c8906c78fc
Timothy Nibert <docz2a@gmail.com>
2024-01-13 15:27:58 +1100

    control Duo internal LED via a touch sensor

--- c/led/ledcontrol.c
+++ c/led/ledcontrol.c
@@ -3,9 +3,10 @@
 
 #include <wiringx.h>
 
-int main() {
-    int LED_PIN = 0;
+#define LED_PIN 25
+#define TOUCH_SENSOR_PIN 16
 
+int main() {
     if(wiringXSetup("duo", NULL) == -1) {
         wiringXGC();
         return -1;
@@ -14,16 +15,15 @@ int main() {
     if(wiringXValidGPIO(LED_PIN) != 0) {
         printf("Invalid GPIO %d\n", LED_PIN);
     }
+    if(wiringXValidGPIO(TOUCH_SENSOR_PIN) != 0) {
+        printf("Invalid GPIO %d\n", LED_PIN);
+    }
 
     pinMode(LED_PIN, PINMODE_OUTPUT);
+    pinMode(TOUCH_SENSOR_PIN, PINMODE_INPUT);
 
     while(1) {
-        printf("Duo LED GPIO (wiringX) %d: Low\n", LED_PIN);
-        digitalWrite(LED_PIN, LOW);
-        sleep(5);
-        printf("Duo LED GPIO (wiringX) %d: High\n", LED_PIN);
-        digitalWrite(LED_PIN, HIGH);
-        sleep(5);
+        digitalWrite(LED_PIN, digitalRead(TOUCH_SENSOR_PIN));
     }
 
     return 0;
